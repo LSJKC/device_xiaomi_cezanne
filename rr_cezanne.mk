e2c92a109e4fcaa69eed1b0572a4bffb6e10dd41
LSJKC <77952988+LSJKC@users.noreply.github.com>
2021-01-25 21:16:50 +0800

    Update rr_cezanne.mk

--- rr_cezanne.mk
+++ rr_cezanne.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Inherit from cezanne device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
